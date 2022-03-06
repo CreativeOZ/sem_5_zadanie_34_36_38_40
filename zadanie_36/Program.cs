da4d541d53369d3a0a2a298145b1fce97b7ac3fd
CreativeOZ <zagriaoleg@gmail.com>
2022-03-07 02:13:23 +0300

    // 36. Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел

---
+++ zadanie_36/Program.cs
@@ -0,0 +1,21 @@
+﻿// 36. Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел
+
+Console.Clear();
+
+int[] array = new int [30];
+int honest = 0;
+int odd = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 999);
+    Console.Write(array[i] + ",\t");
+    if (array[i] % 2 == 0)
+    {
+        honest = honest + 1;
+    }
+    else 
+    {
+        odd = odd + 1;
+    }
+}
+Console.WriteLine($"\n Колличество четных = {honest} \t Колличество нечетных = {odd}");
