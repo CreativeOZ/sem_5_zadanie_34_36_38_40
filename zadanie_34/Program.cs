06c7428e6b8a12c3f3ad3d39fbbd392b310ef562
CreativeOZ <zagriaoleg@gmail.com>
2022-03-07 01:22:56 +0300

    // 34. Написать программу замену элементов массива на противоположные

---
+++ zadanie_34/Program.cs
@@ -0,0 +1,11 @@
+﻿// 34. Написать программу замену элементов массива на противоположные
+
+Console.Clear();
+
+int[] array = {8, 7, 6, -5, -4, 3, 0};
+Console.Write("Противоположные элементы:\t");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = array[i] * -1;
+    Console.Write(array[i] + ",\t");
+}
